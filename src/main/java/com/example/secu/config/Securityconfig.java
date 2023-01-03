9f9bdfbb00917bdc3c7c2f641aa49ccd793b15cf
59013-36-04 <59013-36-04@exchange.ad.afpanet>
2023-01-03 15:57:05 +0100

    V2 utilise h2 commr base de donnes

--- src/main/java/com/example/secu/config/Securityconfig.java
+++ src/main/java/com/example/secu/config/Securityconfig.java
@@ -1,11 +1,16 @@
 package com.example.secu.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -80,8 +85,23 @@ public class Securityconfig {
     }
     @Bean
     public JdbcUserDetailsManager user(DataSource dataSource){
+        UserDetails admin=User.builder()
+                .password("111")
+                .username("aa")
+                .roles("ADMIN")
+                .build();
         JdbcUserDetailsManager jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    EmbeddedDatabase deatasorce(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("dashboard")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 
 }
